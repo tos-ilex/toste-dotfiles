5aedc493938c2b7edb2bc88785ab8a6aa5a50c68
Timo Ostermann <timo.ostermann@zvoove.org>
2023-03-04 17:43:22 +0000

    add xrandr to package bootstrap script

--- install_packages.sh
+++ install_packages.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-pacman -Sy ttf-iosevka-nerd i3-wm i3status yq firefox xclip feh xorg-server neovim alacritty go polybar picom
+pacman -Sy ttf-iosevka-nerd i3-wm i3status yq firefox xclip feh xorg-server neovim alacritty go polybar picom xorg-xrandr
